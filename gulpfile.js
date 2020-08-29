e36adc9b4db6417589e527bc8e1ac8d03ea06069
Kotaina <kotaina217+git@gmail.com>
2020-08-29 13:50:46 +0300

    Final

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var sourcemap = require("gulp-sourcemaps");
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('source/sass/style.scss')
+        .pipe(plumber())
+        .pipe(sourcemap.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
         .pipe(gulp.dest('source/css'));
 });
+
+gulp.task("start", gulp.series("sass"));
